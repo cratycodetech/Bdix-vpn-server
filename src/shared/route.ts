7faca2a8f8571aea964324f739f9cd0a464d98ad
nusratalam123 <nusratalam975@gmail.com>
2024-11-19 16:16:19 +0600

    initial set up

---
+++ src/shared/route.ts
@@ -0,0 +1,34 @@
+import { Router } from "express";
+import authRoutes from "./../routes/auth.route";
+import serverRoutes from "./../routes/server.route";
+
+
+const router = Router();
+
+// Root route
+router.get("/", (_, res) => {
+  res.send("App Working successfully");
+});
+
+// general Routes
+router.use("/auth", authRoutes);
+router.use("/server", serverRoutes);
+
+
+
+// Handle not found
+router.use((req, res, next) => {
+  res.status(404).json({
+    success: false,
+    message: "Not Found",
+    errorMessages: [
+      {
+        path: req.originalUrl,
+        message: "API Not Found",
+      },
+    ],
+  });
+  next();
+});
+
+export default router;
