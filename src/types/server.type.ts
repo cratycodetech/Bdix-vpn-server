5e8cd6bb0ac7bbcd7cacc54417354f0bc2dec9b2
nusratalam123 <nusratalam975@gmail.com>
2024-11-20 16:29:38 +0600

    update server type

--- src/types/server.type.ts
+++ src/types/server.type.ts
@@ -1,11 +1,11 @@
 export type Server= {
-    _id: string; // MongoDB generated ID
-    serverName: string; // Name of the server
-    serverLocation: string; // Location of the server
-    CPUallocation?: string; // Optional CPU allocation
-    memoryAllocation?: string; // Optional memory allocation
-    ipAddress: string; // Server's IP address
-    serverTag?: 'Instagram' | 'Google ads' ; // Enum with default value 'online'
+    _id: string; 
+    serverName: string; 
+    serverLocation: string; 
+    CPUallocation?: string; 
+    memoryAllocation?: string; 
+    ipAddress: string; 
+    serverTag?: 'Instagram' | 'Google ads' ; 
     status:'active' | 'inactive';
   }
   
