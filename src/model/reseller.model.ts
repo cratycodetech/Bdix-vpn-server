acb7dbfe11d966f3a423250bdfa800aaa8b4c84f
nusratalam123 <nusratalam975@gmail.com>
2024-12-02 22:40:19 +0600

    update reseller model

--- src/model/reseller.model.ts
+++ src/model/reseller.model.ts
@@ -14,11 +14,28 @@ const resellerSchema = new Schema<ResellerType>(
       type: Number,
       default: 0,
     },
+    transferCredit:{
+        type: Number,
+        default: 0
+    },
+    availableCredit:{
+      type: Number,
+      default: 0
+  }
   },
   {
     timestamps: true,
   }
 );
+
+// Pre-save hook to ensure totalCredit aligns with the initial credit value
+resellerSchema.pre("save", function (next) {
+  if (this.isNew) {
+
+    this.availableCredit = this.totalCredit;
+  }
+  next();
+});
 
 const Reseller: Model<ResellerType> = models?.Reseller || model("Reseller", resellerSchema);
 
--- src/types/reseller.type.ts
+++ src/types/reseller.type.ts
@@ -2,6 +2,8 @@ export type Reseller ={
     resellerId: unknown; 
     email: string;
     totalCredit: number;
+    transferCredit:number;
+    availableCredit:number;
     createdAt?: Date; 
     updatedAt?: Date; 
     }
