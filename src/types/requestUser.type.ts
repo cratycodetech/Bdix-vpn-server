f0fd406f1b07d7260218b3fef073073029c3a5c3
nusratalam123 <nusratalam975@gmail.com>
2024-12-03 11:33:10 +0600

    deleted the file and updated the file

--- src/types/requestUser.type.ts
+++
@@ -1,9 +0,0 @@
-
-export type RequestUser = {
-  transactionId: string;
-  userId: unknown; 
-  creditAmount: number; 
-  status: "pending" | "done"; 
-  createdAt?: Date; 
-  updatedAt?: Date;           
-}
