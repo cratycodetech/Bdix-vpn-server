1e0006603bc3e82091a17d58b90353811af4085b
nusratalam123 <nusratalam975@gmail.com>
2024-12-03 10:57:25 +0600

    add request route

--- src/controller/request.controller.ts
+++ src/controller/request.controller.ts
@@ -5,12 +5,36 @@ import PremiumUser from "../model/PremiumUser.model";
 import RequestModel from "../model/request.model";
 
 
-// Controller to get all requests
+//get all credit requests(request to reseller to admin)
 export const getAllRequests = async (req:Request, res:Response, next:NextFunction) => {
   try {
-    const requests = await RequestModel.find(); 
+    const requests = await RequestModel.find().sort({ createdAt: -1 }); ; 
     return res.status(200).json({ success: true, data: requests });
   } catch (error) {
     next(error)
   }
 };
+
+
+// Create a new credit request
+export const createCreditRequest = async (req: Request, res: Response) => {
+    try {
+      const { transactionId, resellerId,  creditAmount } = req.body;
+  
+      if (!transactionId || !resellerId || !creditAmount) {
+        return res.status(400).json({ message: "Missing required fields" });
+      }
+  
+      const newRequest = await RequestModel.create(
+       req.body
+      );
+  
+      const savedRequest = await newRequest.save();
+  
+      res.status(201).json({ message: "Request created successfully", data: savedRequest });
+    } catch (error) {
+      console.error("Error creating request:", error);
+      res.status(500).json({ message: "An error occurred while creating the request", error });
+    }
+  };
+  
--- src/routes/request.route.ts
+++ src/routes/request.route.ts
@@ -1,12 +1,18 @@
 import { Router } from "express";
 import {
     getAllRequests,
+    createCreditRequest, 
 } from "../controller/request.controller";
 
 const router = Router();
 
-// Define the route for fetching all requests
+// get all credit requests (request to reseller to admin) 
 router.get('/all', getAllRequests);
+
+// create new Credit request
+router.post("/create", createCreditRequest);
+
+
 
 
 export default router;
